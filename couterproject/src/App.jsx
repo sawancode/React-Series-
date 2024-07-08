5104070b28ad302bf15e1aeb998571b88a508d5a
sawan solanki <sawansolanki121213@gmail.com>
2024-07-08 21:28:30 +0530

    Counter Project

---
+++ couterproject/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+
+  const [couter, setCouter] = useState(0);
+
+  const addValue = () => {
+    setCouter(couter+1)
+  }
+
+  const removeValue = () => {
+    if(couter>0) {
+      setCouter(couter-1)
+    } 
+  }
+
+  return (
+    <>
+      <h1>Counter Project</h1>     
+      <h2>Couter Value: {couter}</h2>
+
+      <button onClick={addValue}>Add Value {couter}</button>
+      <br />
+      <button onClick={removeValue}>Remove Value {couter}</button>
+
+      <p>Footer: {couter}</p>
+    </>
+  )
+}
+
+export default App
