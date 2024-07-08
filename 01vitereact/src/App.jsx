b4c28ebd78343fad9f289600caab7f22245457aa
sawan solanki <sawansolanki121213@gmail.com>
2024-07-08 20:35:35 +0530

    Vite Project and Costom React

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const a = 1000;
+
+  return (
+    <>
+      <h1>Custon App {a}</h1>
+    </>
+  )
+}
+
+export default App
