4459c1b6c09621fbcce068bafa0c3b3cf7ccdb1b
sawan solanki <sawansolanki121213@gmail.com>
2024-07-09 14:41:53 +0530

    Currency Convirtor

---
+++ currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchCurrencyData = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(
+          `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+        );
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCurrencyData();
+  }, [currency]);
+
+  return { data, loading, error };
+}
+
+export default useCurrencyInfo;
