d4a08c1b2ee2ea2de22c572302598782129d1f8f
sawan solanki <sawansolanki121213@gmail.com>
2024-07-09 23:08:55 +0530

    ThemeSwitcher Project

---
+++ themeSwitcher/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// in themeswicher we can add a darkMode: "class" when we can change a theme.
